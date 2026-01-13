bbe7f58b1f6ce9abdc9dc2c8e4af2c7f83980abd
Fernando <ferchin911@hotmail.com>
2026-01-12 19:05:04 -0600

    cambio de proveedor de correo a resend

--- src/app/componentes/contact/contact.component.ts
+++ src/app/componentes/contact/contact.component.ts
@@ -1,46 +1,63 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core'; // <--- 1. Importamos inject
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule,],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  // 2. Inyectamos las dependencias aquí directamente
+  private fb = inject(FormBuilder);
+  private http = inject(HttpClient);
+
   contactForm: FormGroup;
   isSubmitting = signal(false);
   emailCopied = signal(false);
 
-  constructor(private fb: FormBuilder) {
+  constructor() {
+    // 3. El constructor queda más limpio, solo para lógica de inicio
     this.contactForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(2)]],
       email: ['', [Validators.required, Validators.email]],
-      message: ['', [Validators.required, Validators.minLength(10)]]
+      message: ['', [Validators.required, Validators.minLength(5)]]
     });
   }
 
   onSubmit() {
     if (this.contactForm.valid) {
       this.isSubmitting.set(true);
-      // Aquí iría tu lógica de envío (ej: EmailJS o Backend)
-      setTimeout(() => {
-        this.isSubmitting.set(false);
-        this.contactForm.reset();
-        alert('¡Mensaje enviado con estilo! 🚀');
-      }, 2000);
+      const formData = this.contactForm.value;
+
+      // TU URL DE RENDER CON RESEND
+      const backendUrl = 'https://backend-portafolio-43oa.onrender.com/send-email';
+
+      this.http.post(backendUrl, formData).subscribe({
+        next: () => {
+          this.isSubmitting.set(false);
+          this.contactForm.reset();
+          alert('¡Recibido! Estaré en contacto pronto. 🚀');
+        },
+        error: (err) => {
+          console.error(err);
+          this.isSubmitting.set(false);
+          alert('Hubo un error técnico. Intenta por WhatsApp.');
+        }
+      });
     } else {
       this.contactForm.markAllAsTouched();
     }
   }
 
   copyEmail() {
-    const email = 'tuemail@gmail.com'; // Pon tu correo real aquí
+    const email = 'cfarzate@gmail.com'; 
     navigator.clipboard.writeText(email).then(() => {
       this.emailCopied.set(true);
-      setTimeout(() => this.emailCopied.set(false), 2000); // Reset mensaje después de 2s
+      setTimeout(() => this.emailCopied.set(false), 2000);
     });
   }
 }
